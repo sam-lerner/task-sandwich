c929ffd90a9a8bf14ea0bb9322c25625ec7541a2
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-09 15:35:37 -0500

    server.js change

--- server/server.js
+++ server/server.js
@@ -1,39 +1,18 @@
 const express = require('express');
-// const { ApolloServer } = require('@apollo/server');
 const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
 
-app.use(express.urlencoded({ extended: false })); // true or false?
+let cors = require("cors");
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-// const startApolloServer = async (typeDefs, resolvers) => {
-  // await server.start();
-  // server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`🍔 API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath} 🧙`);
-    })
-  })
-  // };
-  
-  // startApolloServer(typeDefs, resolvers);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
