3c88cb787f0952b679e9fbad7c37d1775dc9da7a
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-09 15:25:49 -0500

    Heroku or sandwich

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
+// const { ApolloServer } = require('@apollo/server');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
