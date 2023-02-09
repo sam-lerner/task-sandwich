5ef3b9fa9640981aec5f1b56b650e8302100b954
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-02-09 13:17:52 -0500

    added models to User, Teams, Projects, and Tasks

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
 
@@ -8,7 +9,42 @@ const userSchema = new Schema({
     unique: true,
     trim: true,
   },
+
+  email: {
+    type: String,
+    required:true,
+    unique: true,
+    trim: true
+  },
+
+  password: {
+    type:String,
+    required: true,
+    trim: true,
+    min_length: 8
+  },
+  teams: [/*teams*/],
+
+  projects: [/*projects*/],
+
+  sandwichCount: {
+    type: Int16Array
+  }
+
 });
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
