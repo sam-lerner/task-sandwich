7aeb1aed77f8712a639a9c954d9c6c6ddd63e651
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-10 10:31:03 -0500

    i can commit i promise

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+let cors = require("cors");
+app.use(cors());
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-if (process.env.KEY === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
