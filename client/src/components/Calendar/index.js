2a63b21ed215bd08f74238e63afe48a9a18d4b30
azimezn <117550865+azimezn@users.noreply.github.com>
2023-02-10 14:22:01 -0500

    calendar is now react acceptable. calendar buttons are working, but not correctly

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,103 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import './style.css';
-import { Container, Row, Col } from 'react-bootstrap';
 
-// calendar variables
-let today = dayjs();
-let currentMonth
-// const calendar = document.querySelector("#calendar");
-let dateDisplay
-let calendarBody
+const Calendar = () => {
 
-// calendar event form
-// const eventForm = document.querySelector("#event");
-// const eventDate = document.querySelector("#eventDate");
-// const addEventButton = document.querySelector("#addEvent");
+    // calendar variables
+    const currentDate = dayjs();
+    const dateDisplay = currentDate.format('MMMM D, YYYY')
+    const [today, setToday] = useState(dayjs());
+    const [currentMonth, setCurrentMonth] = useState(today.format('MMMM'));
+    const [calendarBody, setCalendarBody] = useState('');
 
-let events = [];
+    // calendar event form
+    // const eventForm = document.querySelector("#event");
+    // const [event, setEvent ] = useState("";)
+    // const eventDate = document.querySelector("#eventDate");
+    // const addEventButton = document.querySelector("#addEvent");
 
-function init() {
-    dateDisplay = today.format('MMMM D, YYYY')
-    currentMonth = today.format('MMMM')
-    generateCalendar(today);
-}
+    let events = [];
 
-function generateCalendar(date) {
-    calendarBody = "";
-    const firstDate = dayjs(date).startOf('month');
-    const lastDate = dayjs(date).endOf('month');
-    // .day: days of the week - what day is first day of the month in number
-    let startDay = firstDate.day();
-    // .date: days of the month - what day is last day of the month in number
-    let daysInMonth = lastDate.date();
+    // rerun {generateCalendar} when {today} changes
+    useEffect(() => {
+        generateCalendar(today);
+    });
 
-    // starting from the 1st day of the month
-    let dateCounter = 1;
-    let rows = "";
+    function generateCalendar(date) {
+        const firstDate = dayjs(date).startOf('month');
+        const lastDate = dayjs(date).endOf('month');
+        // .day: days of the week - what day is first day of the month in number
+        let startDay = firstDate.day();
+        // .date: days of the month - what day is last day of the month in number
+        let daysInMonth = lastDate.date();
 
-    while (dateCounter <= daysInMonth) {
-        let cells = "";
-        for (let i = 0; i < 7; i++) {
-            // creates empty spots of first week. startDay starts from the beginning of the week and counts down to the first day of the month.
-            if (startDay > 0) {
-                cells += "<td></td>";
-                // console.log(`startDay: ${startDay}, cells: ${cells}`)
-                startDay--;
-                // creates the rest of the month
-                // adds to dateCounter at the end. if dateCounter is less than total days of the month, continue
-            } else if (dateCounter <= daysInMonth) {
-                // console.log(`dateCounter: ${dateCounter}, daysInMonth: ${daysInMonth}`)
-                // !!! currently no events
-                const event = events.find(e => e.date.date() === dateCounter && e.date.month() === date.month());
-                cells += `<td data-date="${dateCounter}">${event ? event.event : dateCounter}</td>`;
-                // console.log(`cells: ${cells}`);
-                dateCounter++;
+        // starting from the 1st day of the month
+        let dateCounter = 1;
+        let rows = "";
+
+        while (dateCounter <= daysInMonth) {
+            let cells = "";
+            for (let i = 0; i < 7; i++) {
+                // creates empty spots of first week. startDay starts from the beginning of the week and counts down to the first day of the month.
+                if (startDay > 0) {
+                    cells += "<td></td>";
+                    startDay--;
+                    // creates the rest of the month
+                    // adds to dateCounter at the end. if dateCounter is less than total days of the month, continue
+                } else if (dateCounter <= daysInMonth) {
+                    // !!! currently no events
+                    const event = events.find(e => e.date.date() === dateCounter && e.date.month() === date.month());
+                    cells += `<td data-date="${dateCounter}">${event ? event.event : dateCounter}</td>`;
+                    dateCounter++;
+                }
             }
+            rows += `<tr>${cells}</tr>`;
         }
-        rows += `<tr>${cells}</tr>`;
+        setCalendarBody(rows);
     }
 
-    calendarBody = rows;
-    console.log(calendarBody)
-}
+    function previous() {
+        setToday(today.subtract(1, "month"));
+        setCurrentMonth(today.format('MMMM'));
+        generateCalendar(today);
+    }
 
+    function next() {
+        setToday(today.add(1, "month"));
+        setCurrentMonth(today.format('MMMM'));
+        generateCalendar(today);
+    }
 
-function previous() {
-    today = today.subtract(1, "month");
-    currentMonth = today.format('MMMM');
-    generateCalendar(today);
-}
-
-// previous.addEventListener("click", () => {
-//     today = today.subtract(1, "month");
-//     currentMonth.textContent = today.format('MMMM');
-//     generateCalendar(today);
-// });
-
-function next() {
-    today = today.add(1, "month");
-    currentMonth = today.format('MMMM');
-    generateCalendar(today);
-}
-
-// next.addEventListener("click", () => {
-//     today = today.add(1, "month");
-//     currentMonth.textContent = today.format('MMMM');
-//     generateCalendar(today);
-// });
-
-// addEventButton.addEventListener("click", () => {
-//     const event = eventForm.value;
-//     const date = new Date(eventDate.value);
-
-//     events.push({ event, date });
-//     generateCalendar(today);
-// });
-
-init();
-
-const Profile = () => {
+    // addEventButton.addEventListener("click", () => {
+    //     const event = eventForm.value;
+    //     const date = new Date(eventDate.value);
+    //     events.push({ event, date });
+    //     generateCalendar(today);
+    // });
 
     return (
         <>
@@ -105,10 +82,11 @@ const Profile = () => {
             <div id="calendar">
                 <div id="calendarHeader">
                     {/* &lt; = less than symbol */}
-                    <button onClick={() => previous()} id="previous">&lt;</button>
+                    {/* <button onClick={() => previous()} id="previous">&lt;</button> */}
+                    <button onClick={previous}>&lt;</button>
                     <h3 id="currentMonth">{currentMonth}</h3>
                     {/* &gt; = greater than symbol */}
-                    <button onClick={() => next()} id="next">&gt;</button>
+                    <button onClick={next}>&gt;</button>
                 </div>
                 <table>
                     {/* thead: table head */}
@@ -127,6 +105,9 @@ const Profile = () => {
                     </thead>
                     {/* tbody: thread body */}
                     <tbody id="calendarBody" dangerouslySetInnerHTML={{ __html: calendarBody }}></tbody>
+                    {/* <tbody id="calendarBody">
+                        {calendarBody}
+                    </tbody> */}
                 </table>
             </div>
             {/* <form id="eventForm">
@@ -139,4 +120,4 @@ const Profile = () => {
 
 };
 
-export default Profile;
+export default Calendar;
