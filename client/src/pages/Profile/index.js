66ab441e0a7a2950d1ef1cd2cb5c4032855b6711
azimezn <117550865+azimezn@users.noreply.github.com>
2023-02-10 20:24:47 -0500

    worked on mutation.js, queries.js, auth.js in client side

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { Calendar } from '../../components';
+import Auth from '../../utils/auth';
 
+import { useQuery } from '@apollo/client';
+import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
 
 const Profile = () => {
+  const { userId } = useParams();
+
+  // // If there is no `userId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+  // const { loading, data } = useQuery(
+  //   userId ? QUERY_SINGLE_USER : QUERY_ME,
+  //   {
+  //     variables: { userId: userId },
+  //   }
+  // );
 
 
-return (
+  // // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
+  // const user = data?.me || data?.user || {};
+
+  // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getUser().data._id === userId) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.name) {
+    return (
+      <h4>
+        You need to be logged in to see your profile page. Use the navigation
+        links above to sign up or log in!
+      </h4>
+    );
+  }
+
+  return (
     <>
       <Calendar />
+      {/* isLoggedInUser={!userId && true} */}
     </>
-)
+  )
 
 };
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,14 +1,46 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
 
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
 
-// !!! example
-// export const ADD_PROFILE = gql`
-//   mutation addProfile($name: String!) {
-//     addProfile(name: $name) {
-//       _id
-//       name
-//       skills
-//     }
-//   }
-// `;
+export const ADD_TEAM = gql`
+  mutation addTeam($userId: ID!, $teamName: String!, $members: String, $projects: String) {
+    addTeam(userId: $userId, teamName: $teamName, members: $members, projects: $projects) {
+      _id
+      teamName
+      admin
+      members
+      project
+    }
+  }
+`;
+
+export const ADD_PROJECT = gql`
+  mutation addProject($userId: ID!, $projectName: String!) {
+    addProject(userId: $userId, projectName: $projectName) {
+      _id
+      projectName
+    }
+  }
+`;
