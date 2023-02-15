ab2255f4ab2d150f1455d803e60853aaf7fd36ee
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-02-15 13:49:59 -0500

    added gifs and descriptions to homepage

--- client/src/pages/Home/images/index.js
+++ client/src/pages/Home/images/index.js
@@ -1,4 +1,7 @@
 import sandwichLogoTop from "./sandwich-logo-top.jpg";
 import sandwichLogoBottom from "./sandwich-logo-bottom.jpg";
+import taskGif from "./task-gif.gif"
+import teamGif from "./team-gif.gif"
+import sandwichGif from "./sandwich-gif.gif"
 
-export { sandwichLogoTop, sandwichLogoBottom };
+export { sandwichLogoTop, sandwichLogoBottom, taskGif, teamGif, sandwichGif };
