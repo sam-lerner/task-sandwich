8151989422de8fedfbde3d8f76d4a68ee85433a8
Eric K <erickirberger@gmail.com>
2023-02-15 18:15:42 -0500

    header title is at least stable, but can't do it easily using google fonts yet...

--- client/src/components/MainComponents/Header/index.js
+++ client/src/components/MainComponents/Header/index.js
@@ -5,21 +5,13 @@ import { sandwichLogoTopEdited, sandwichLogoBottomEdited } from './images';
 
 const Header = () => {
   return (
-    // <header className="image-wrapper">
-    //   <img className="top-image" src={sandwichLogoTopEdited} alt="Tasty-looking sandwich top."></img>
-    //   <a href="/" className="title-link">
-    //     <div className="image-wrapper">
-    //       <h1 className="title-name">TASK SANDWICH</h1>
-    //       <h2 className="title-name">"Except No Subs!!"</h2>
-    //     </div>
-    //   </a>
-    //   <img className="bottom-image" src={sandwichLogoBottomEdited} alt="Tasy-looking sandwich bottom."></img>
-    // </header>
     <header className="image-wrapper">
       <div childrenlassName="sandwich-wrapper">
         <img className="top-image" src={sandwichLogoTopEdited} alt="Tasty-looking sandwich top."></img>
-        <a href="/" className="title-link"><h1 className="title-name">TASK SANDWICH</h1></a>
-        <a href="/" className="title-link"><p className="title-slogan">"Accept No Subs!!"</p></a>
+        <a href="/" className="title-link">
+          <h1 className="title-name">TASK SANDWICH</h1>
+          <h6 className="title-slogan">"Accept No Subs!!"</h6>
+        </a>
         <img className="bottom-image" src={sandwichLogoBottomEdited} alt="Tasy-looking sandwich bottom."></img>
       </div>
     </header>
