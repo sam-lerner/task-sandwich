666780ed5275cb22bed24a5f4e8e837668f66fab
Eric K <erickirberger@gmail.com>
2023-02-15 22:17:33 -0500

    base 404 styling

--- client/src/pages/BadNav/index.js
+++ client/src/pages/BadNav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { Container } from "react-bootstrap";
 
 import { badnav } from "./images";
 
@@ -8,14 +9,24 @@ import "./style.css";
 function BadNav() {
   let location = useLocation();
   return (
-    <div className="badnav-top-card card bg-white card-rounded w-50">
-      <div className="badnav-card card-header bg-dark text-center">
-      <img src={badnav} className="badnav" alt="An empty plate 404."></img>
-        <h1 className="badnav-text">
-          No sandwiches to be had at this address: <code>{location.pathname}</code>
-        </h1>
+    // <Container>
+      <div className="badnav-top-card">
+        <div className="badnav-card card-header bg-dark text-center">
+        <img src={badnav} className="badnav" alt="An empty plate 404."></img>
+          <h1 className="badnav-text">
+            No sandwiches to be had at this address: <code>{location.pathname}</code>
+          </h1>
+        </div>
       </div>
-    </div>
+      // {/* <div className="badnav-top-card card bg-white card-rounded w-50">
+      //   <div className="badnav-card card-header bg-dark text-center">
+      //   <img src={badnav} className="badnav" alt="An empty plate 404."></img>
+      //     <h1 className="badnav-text">
+      //       No sandwiches to be had at this address: <code>{location.pathname}</code>
+      //     </h1>
+      //   </div>
+      // </div> */}
+    // </Container>
   );
 }
 
